0437c5b638b0b3a1bd0db25024f4dcfe1caf7a9d
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-03 00:50:30 +0300

    deletes irrelevant

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import React from "react";
+import { DataProvider } from "./contexts/DataContext";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Profile from "./components/Profile";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <DataProvider>
+      <div>APP </div>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Profile />
+      <Contact />
+    </DataProvider>
+  );
 }
 
 export default App;
