160ad903a82847674d5a8c3524e2b8154370fdeb
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-03 16:40:16 +0300

    creates language context

---
+++ web-sayfam/src/contexts/LanguageContext.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState, useContext } from "react";
+
+export const LanguageContext = createContext();
+export const useLanguage = () => useContext(LanguageContext);
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("en");
+
+  const toggleLanguage = () => {
+    setLanguage((lang) => (lang === "en" ? "tr" : "en"));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
