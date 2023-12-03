d03cc7f9f116f700ad248488915c1ce4eccbd169
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-03 16:34:36 +0300

    adds icons

--- web-sayfam/src/components/Contact.js
+++ web-sayfam/src/components/Contact.js
@@ -1,5 +1,12 @@
 import React, { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faTwitter,
+  faCodepen,
+} from "@fortawesome/free-brands-svg-icons";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
   const { postData } = useContext(DataContext);
@@ -9,6 +16,10 @@ const Contact = () => {
       <h3>{contact.title}</h3>
       <p>{contact.text}</p>
       <p> {contact.email}</p>
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faCodepen} />
+        <FontAwesomeIcon icon={faAt} />
+        <FontAwesomeIcon icon={faInstagram} />
     </div>
   );
 };
