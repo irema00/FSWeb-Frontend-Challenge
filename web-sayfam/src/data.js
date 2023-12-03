d2bb57ec6493d57e4687f72fdb5ce9b1562a378c
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-03 16:47:04 +0300

    updates images

--- web-sayfam/src/data.js
+++ web-sayfam/src/data.js
@@ -1,7 +1,7 @@
 export const engData = {
   name: "almila",
   header: "I am a Frontend Developer",
-  imageURL: "https://picsum.photos/200",
+  heroRight: "/images/hero-right.png",
   description:
     "... who likes to craft solid and scalable frontend products with great user experiences",
   links: ["Github", " Linkedin"],
@@ -21,12 +21,23 @@ export const engData = {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent accumsan, nisi in vehicula varius, eros leo fermentum orci, ac tincidunt ipsum magna eu nulla. Mauris ut molestie nisl, eget pulvinar tellus. Nulla tincidunt ornare massa, non pellentesque libero venenatis pretium. Morbi eu lectus semper, fringilla metus a, sagittis mauris. ",
     "Phasellus tristique lacus neque, laoreet fermentum metus maximus luctus. Donec rutrum tellus vel augue tincidunt fringilla. Donec faucibus lacus in justo vulputate imperdiet. Proin tristique malesuada lectus sed semper. Suspendisse potenti. ",
   ],
-  skills: ["JAVASCRIPT", "REACT", "REDUX", "NODE", "REACT", "VS CODE", "FIGMA"],
+  profileImg: "/images/profile.png",
+  skills: {
+    JavaScript: "/images/js.png",
+    React: "/images/react.png",
+    Redux: "/images/redux.png",
+    NodeJS: "/images/nodejs.png",
+    VSCode: "/images/VScode.png",
+    Figma: "/images/figma.png",
+  },
+
   projects: ["Workintech", "Journey"],
   projectsText: [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In neque ante, lobortis vitae tincidunt in, volutpat quis lacus. Sed sollicitudin nisi elit, vel auctor est cursus non. Quisque eu efficitur felis. Cras nisl ligula, tincidunt nec nulla nec, ultrices fringilla nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac suscipit arcu. Nullam nec tellus vitae erat iaculis varius ut eu libero. Nam lectus massa, malesuada in dignissim eu, semper a ipsum. ",
     "Cras quis tortor in velit porta ultricies sed ut arcu. Proin volutpat nisl sapien, eget elementum nibh imperdiet et. Maecenas mi mi, maximus quis velit et, eleifend hendrerit nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vel elementum eros. Donec risus mauris, scelerisque et purus maximus, ultrices sodales nibh. Vestibulum aliquet, tortor eu sagittis varius, metus nulla finibus nulla, quis pulvinar diam dui vel nisl. ",
   ],
+  projectsImg: ["/images/Rectangle1.png", "/images/Rectangle2.png"],
+
   contact: {
     title: "Send me a message!",
     text: "Got a question or proposal, or just want to say hello? Go ahead.",
