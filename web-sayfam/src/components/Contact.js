05642d5db90d28ef7a5fd056c299023805f81bb8
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:22:20 +0300

    checks data to render and add icons

--- web-sayfam/src/components/Contact.js
+++ web-sayfam/src/components/Contact.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useData } from "../contexts/DataContext";
 import { useTheme } from "../contexts/ThemeContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,6 +13,9 @@ const Contact = () => {
   const { postData } = useData();
   const { contact } = postData;
   const { theme } = useTheme();
+  if (!postData || !postData.contact) {
+    return <div>Loading...</div>;
+  }
   return (
     <div
       className={`flex justify-center py-20 ${
@@ -20,6 +24,7 @@ const Contact = () => {
           : "bg-[#F9F9F9] text-[#4731D3]"
       }`}
     >
+      <div className="sm:w-1/3">
         <h3 className="text-[48px] font-bold">{contact.title}</h3>
         <p
           className={`text-[30px] px-12 leading-10 pt-5 ${
@@ -29,6 +34,12 @@ const Contact = () => {
           {contact.text}
         </p>
         <p className="pt-10 text-xl underline">{contact.email}</p>
+        <div className="px-5 justify-center space-x-4 mt-4 ">
+          <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
+          <FontAwesomeIcon icon={faCodepen} className="text-2xl" />
+          <FontAwesomeIcon icon={faAt} className="text-2xl" />
+          <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
+        </div>
       </div>
     </div>
   );
