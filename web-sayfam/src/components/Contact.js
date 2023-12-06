2c7941adb5689ce31cd7485f7b92a7af81ebb42f
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:20:45 +0300

    adds theme context

--- web-sayfam/src/components/Contact.js
+++ web-sayfam/src/components/Contact.js
@@ -1,4 +1,5 @@
 import { useData } from "../contexts/DataContext";
+import { useTheme } from "../contexts/ThemeContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
@@ -10,8 +11,23 @@ import { faAt } from "@fortawesome/free-solid-svg-icons";
 const Contact = () => {
   const { postData } = useData();
   const { contact } = postData;
+  const { theme } = useTheme();
   return (
+    <div
+      className={`flex justify-center py-20 ${
+        theme === "dark"
+          ? "bg-[#252128] text-[#8F88FF]"
+          : "bg-[#F9F9F9] text-[#4731D3]"
+      }`}
+    >
         <h3 className="text-[48px] font-bold">{contact.title}</h3>
+        <p
+          className={`text-[30px] px-12 leading-10 pt-5 ${
+            theme === "dark" ? " text-[#FFFFFF]" : "text-[#120B39]"
+          }`}
+        >
+          {contact.text}
+        </p>
         <p className="pt-10 text-xl underline">{contact.email}</p>
       </div>
     </div>
