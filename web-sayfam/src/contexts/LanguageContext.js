f29f9b54d8ec07efc8f405b45d971c29bdb5de83
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:29:55 +0300

    adds lang preference to local storage

--- web-sayfam/src/contexts/LanguageContext.js
+++ web-sayfam/src/contexts/LanguageContext.js
@@ -1,14 +1,19 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 export const LanguageContext = createContext();
-export const useLanguage = () => useContext(LanguageContext);
-export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState("en");
 
+export const useLanguage = () => useContext(LanguageContext);
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState(
+    localStorage.getItem("language") ? localStorage.getItem("language") : "en"
+  );
   const toggleLanguage = () => {
     setLanguage((lang) => (lang === "en" ? "tr" : "en"));
   };
-
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
   return (
     <LanguageContext.Provider value={{ language, toggleLanguage }}>
       {children}
