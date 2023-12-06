a74821efad902213214bc1cf354f26b1b926f8f8
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 08:17:27 +0300

    forms files for future switch to redux

---
+++ web-sayfam/src/actions/actions.js
@@ -0,0 +1,9 @@
+// export const setTheme = (theme) => ({
+//   type: 'SET_THEME',
+//   payload: theme,
+// });
+
+// export const setLanguage = (language) => ({
+//   type: 'SET_LANGUAGE',
+//   payload: language,
+// });
