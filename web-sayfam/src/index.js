ee4dc05ad9d422d2135b7ddd34d0058ec7ac9d33
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:14:07 +0300

    wraps app.js with providers

--- web-sayfam/src/index.js
+++ web-sayfam/src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { DataProvider } from "./contexts/DataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <LanguageProvider>
+    <DataProvider>
+      <ThemeProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ThemeProvider>
+    </DataProvider>
+  </LanguageProvider>
 );
