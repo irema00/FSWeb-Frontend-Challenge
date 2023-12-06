e615859efd18f1a7acbca8862cf773f8dfe59888
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:23:33 +0300

    creates hero component and styles with tailwind

--- web-sayfam/src/components/Hero.js
+++ web-sayfam/src/components/Hero.js
@@ -1,50 +1,93 @@
-import React, { useContext, useEffect } from "react";
-import { DataContext } from "../contexts/DataContext";
-import Header from "./Header";
-import LanguageSwitch from "./LanguageSwitch";
-import { LanguageContext } from "../contexts/LanguageContext";
-import { engData, trData } from "../data";
 import "../App.css";
+import React, { useEffect } from "react";
+
+import Header from "./Header";
+import LanguageSwitch from "../utils/LanguageSwitch";
+import ThemeToggle from "../utils/ThemeToggle";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
+import { useData } from "../contexts/DataContext";
+import { useTheme } from "../contexts/ThemeContext";
+import { useLanguage } from "../contexts/LanguageContext";
+import { engData, trData } from "../data";
+
 const Hero = () => {
-  const { postData, setPostData, loading, error } = useContext(DataContext);
-  const { language } = useContext(LanguageContext);
+  const { postData, setPostData } = useData();
   const { description, heroRight, header, links } = postData;
+  const { theme } = useTheme();
+  const { language } = useLanguage();
+
   useEffect(() => {
     const dataToSend = language === "en" ? engData : trData;
     setPostData(dataToSend);
   }, [language, setPostData]);
-  return (
-    <div className="hero">
-      <Header /> <LanguageSwitch /> <div>Dark Mode / Light Mode</div>
-      <div className="hero-content">
-        <div className="hero-left">
-          <h1>{header}</h1>
-          <p>{description}</p>
-          <div className="hero-btn">
-            {links &&
-              links.map((link, index) => (
-                <div key={index}>
-                  <button className="btn">
-                    {index % 2 === 0 ? (
-                      <FontAwesomeIcon icon={faGithub} size="2x" />
-                    ) : (
-                      <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                    )}
 
-                    <p>{link}</p>
-                  </button>
-                </div>
-              ))}
+  return (
+    <div
+      className={`hero-container flex flex-wrap-reverse sm:no-wrap justify-center m-auto ${
+        theme === "dark" ? "bg-[#171043]" : "bg-[#4731D3]"
+      }`}
+    >
+      <div className="sm:w-2/3 sm:pl-[15%] sm:pb-20 sm:pt-12 sm:relative ">
+        <div className="hero-header flex flex-col justify-center h-auto ">
+          <div className="header-right  flex flex-row items-end  content-center justify-around sm:justify-end sm:pl-[55%] sm:pr-[5%]">
+            <LanguageSwitch />
+          </div>
+          <div className="header-left flex justify-center sm:justify-start">
+            <Header />
           </div>
         </div>
-
-        <div className="hero-right">
-          {" "}
-          <img src={heroRight} />
+        <div className="hero-container flex flex-col py-[0] justify-center m-auto">
+          <div className="flex flex-wrap-reverse sm:flex-nowrap sm:flex-row ">
+            <div className="hero-left mb-8 md:mb-0 md:w-1/2 ">
+              <h1 className="font-bold text-[3rem] leading-[59.4px] text-[#CBF281] mx-12 sm:ml-0 sm:mt-20 text-left mb-8">
+                {header}
+              </h1>
+              <p className="font-normal text-[24px] mx-12 sm:ml-0 leading-[30px] text-[#FFFFFF] text-left">
+                {description}
+              </p>
+              <div className="hero-btn flex items-center justify-center sm:justify-start mt-6 flex-row gap-2 md:space-x-4">
+                {links &&
+                  links.map((link, index) => (
+                    <div key={index}>
+                      <button
+                        className={`flex w-[127px] h-[52px] rounded-[6px] border-[1px] border-[solid] pl-[12px] pr-[20px,] py-[12px,] text-[#3730A3] flex-row items-center justify-evenly font-medium text-[18px] leading-[28px] mt-8 ${
+                          theme === "dark"
+                            ? "bg-[#252128] text-[#FFFFFF]"
+                            : "bg-[#FFFFFF] text-[#3730A3]"
+                        }`}
+                      >
+                        {index % 2 === 0 ? (
+                          <FontAwesomeIcon icon={faGithub} size="2x" />
+                        ) : (
+                          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        )}
+                        <p
+                          className={`text-[1rem] ${
+                            theme === "dark"
+                              ? "text-[#FFFFFF]"
+                              : "text-[#3730A3]"
+                          }`}
+                        >
+                          {link}
+                        </p>
+                      </button>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <img
+              className="hero-right max-w-full h-auto sm:absolute top-[100px] -right-[260px]"
+              src={heroRight}
+              alt="woman-working-laptop"
+            />
+          </div>
         </div>
+      </div>
+      <div className="sm:bg-[#CBF281] sm:w-1/3 sm:pr-[20%] sm:pb-20  mb-[10px] sm:mb-0  sm:pt-6">
+        <ThemeToggle />
       </div>
     </div>
   );
