ff18fa73cd55105cacff1c123db16d7af91ef65d
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:24:54 +0300

    creates skills component and styles with tailwind

--- web-sayfam/src/components/Skills.js
+++ web-sayfam/src/components/Skills.js
@@ -1,23 +1,48 @@
-import React, { useContext } from "react";
-import { DataContext } from "../contexts/DataContext";
+import React from "react";
+import { useData } from "../contexts/DataContext";
+import { useTheme } from "../contexts/ThemeContext";
 
 const Skills = () => {
-  const { postData } = useContext(DataContext);
+  const { postData } = useData();
   const { skills } = postData;
+  const { theme } = useTheme();
+
   return (
-    <div className="skills-container">
-      <h2>Skills</h2>
-      <div className="skill-box">
-        <ul>
+    <div
+      className={`flex sm:pl-[15%] sm:py-[5%] pb-8 flex-col sm:flex-row justify-center items-center 
+      sm:items-start 
+       py-[0] sm:gap-[5%] m-auto
+      ${theme === "dark" ? "bg-[#252128]" : "bg-[#FFFFFF]"}`}
+    >
+      <h2
+        className={`font-bold text-[3rem]  ${
+          theme === "dark" ? "text-[#CBF281]" : "text-[#4731D3]"
+        }`}
+      >
+        Skills
+      </h2>
+      <div className="skill-box-container flex items-start ">
+        <ul className="flex list-none flex-wrap sm: sm:flex-wrap  flex-col sm:flex-row sm:items-start">
           {skills &&
             Object.entries(skills).map(([skillName, skillImage], index) => (
-              <li key={index}>
+              <li
+                key={index}
+                className="flex items-center justify-center sm:justify-start flex-col sm:flex-row sm:mr-[50px] min-w-[425px]"
+              >
                 <img
+                  className=" w-[250px] h-auto
+                  "
                   src={skillImage}
                   alt={skillName}
-                  style={{ width: "50px", height: "50px", marginRight: "10px" }}
                 />
-                {skillName}
+
+                <div
+                  className={`text-xl sm:text-xl text-3xl sm:max-h-[720px] ${
+                    theme === "dark" ? "text-[#FFFFFF]" : "text-[#777777]"
+                  }`}
+                >
+                  {skillName}
+                </div>
               </li>
             ))}
         </ul>
