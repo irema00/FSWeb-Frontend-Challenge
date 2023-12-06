43dc0c11c96b3d289749e4160facabd90d3d02a0
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-06 07:14:30 +0300

    moves providers to index.js

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,24 +1,22 @@
 import "./App.css";
+import "./index.css";
 import React from "react";
-import { DataProvider } from "./contexts/DataContext";
+
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Profile from "./components/Profile";
 import Contact from "./components/Contact";
-import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
   return (
-    <LanguageProvider>
-      <DataProvider>
-        <Hero />
-        <Skills />
-        <Profile />
-        <Projects />
-        <Contact />
-      </DataProvider>
-    </LanguageProvider>
+    <div className="app">
+      <Hero />
+      <Skills />
+      <Profile />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
