8e4dab2ad726a0772eb4455751e59dfcb03f73be
İrem Akın Tunga <iremakin00@gmail.com>
2023-12-07 03:16:05 +0300

    adds theme texts

--- web-sayfam/src/utils/ThemeToggle.js
+++ web-sayfam/src/utils/ThemeToggle.js
@@ -5,11 +5,22 @@ function ThemeToggle() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div
-      className={`tdnn ${theme === "light" ? "day" : ""}`}
-      onClick={toggleTheme}
-    >
-      <div className={`moon ${theme === "light" ? "sun" : ""}`}></div>
+    <div className="flex items-baseline gap-2 w-[200px] mt-0 justify-center ">
+      <div
+        className={`tdnn ${theme === "light" ? "day" : ""}`}
+        onClick={toggleTheme}
+      >
+        <div className={`moon ${theme === "light" ? "sun" : ""}`}></div>
+      </div>
+      {theme === "light" ? (
+        <div className="text-indigo-700 text-[15px] font-bold font-['Inter'] tracking-wider">
+          DARK MODE
+        </div>
+      ) : (
+        <div className="text-zinc-300 text-[15px] font-bold font-['Inter'] tracking-wider">
+          LIGHT MODE
+        </div>
+      )}
     </div>
   );
 }
